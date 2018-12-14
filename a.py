9ff9373bd2d29457f0b6ad34610e5322102f06ee
Kir Fog <kirfog@gmail.com>
2018-12-14 23:19:19 +0300

    WORKS

--- a.py
+++ a.py
@@ -1,30 +1,41 @@
-from numpy import *
+import numpy
 z = 3
 #a = zeros((z+2, z+2))
-b = zeros((z+2, z+2))
+#b = numpy.zeros((z+2, z+2))
 
-a=array([[0, 0, 0, 0, 0,],
- 		 [0, 1, 1, 0, 0,],
- 		 [0, 0, 1, 1, 0,],
- 		 [0, 1, 1, 0, 0,],
- 		 [0, 0, 0, 0, 0,]])
+a=numpy.array([[0, 0, 0, 0, 0,],
+ 		 	   [0, 1, 1, 1, 0,],
+ 		 	   [0, 1, 1, 1, 0,],
+ 		 	   [0, 0, 0, 0, 0,],
+ 		 	   [0, 0, 0, 0, 0,]])
 
-def neib(array,z):
+b=numpy.array([[0, 0, 0, 0, 0,],
+ 		 	   [0, 0, 0, 0, 0,],
+ 		 	   [0, 0, 0, 0, 0,],
+ 		 	   [0, 0, 0, 0, 0,],
+ 		 	   [0, 0, 0, 0, 0,]])
+
+print(a)
+
+def tern(a):
 	for x in range(1, z+1):
 		for y in range(1, z+1):
 			c = 0
 			for i in range(x-1, x+2):
 				for j in range(y-1, y+2):
-					if i != j:
+					if not (i == x and j == y):
 						c = c + a[i,j]
-						print(c)
 			if a[x, y] == 0 and c == 3:
 				b[x, y] = 1
-			elif (a[x,y] == 1 and c > 3) or (a[x,y] == 1 and c < 2):
+			elif(a[x,y] == 1 and c > 3) or (a[x,y] == 1 and c < 2):
 				b[x, y] = 0
 			else:
 				b[x, y] = a[x, y]
-	return b
-d = neib(a,z)
-print(d)
+	return(b)
+
+for i in range(1,11):
+	a = tern(a)
+	print(a)
+
+
 
