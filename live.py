2168110826af63f98b961103144f819cfd449189
Kir Fog <kirfog@gmail.com>
2018-12-15 22:50:25 +0300

    xy

--- live.py
+++ live.py
@@ -1,44 +1,54 @@
 import pygame
 import numpy
 
-class GameOfLife:
-    def __init__(self, width = 1000, height = 1000, cell_size = 20, speed = 2):
+class Life:
+    def __init__(self, width = 1000, height = 500, cell_size = 20, speed = 2):
         self.width = width
         self.height = height
         self.cell_size = cell_size
         self.screen_size = width, height
         self.screen = pygame.display.set_mode(self.screen_size)
-        self.cell_width = self.width // self.cell_size
-        self.cell_height = self.height // self.cell_size
+        self.x = self.height // self.cell_size
+        self.y = self.width // self.cell_size
         self.speed = speed
-
         self.cells=[]
 
     def draw_grid(self):
-        for x in range(self.cell_width):
-            pygame.draw.line(self.screen, pygame.Color('black'), (x*self.cell_size, 0), (x*self.cell_size, self.height))
-        for y in range(self.cell_height):
+        for x in range(self.y):
+            pygame.draw.line(self.screen, pygame.Color('black'), (x*self.cell_size, 0), (x*self.cell_size, self.width))
+
+        for y in range(self.x):
             pygame.draw.line(self.screen, pygame.Color('black'), (0, y*self.cell_size), (self.width, y*self.cell_size))
 
-    def cell_list(self, randomize=False):
+    def draw(self, cells):
+        for x in range (self.x):
+            for y in range (self.y):
+                if cells[x,y] == 0:
+                    pygame.draw.rect(self.screen, pygame.Color('white'), ((y)*self.cell_size,(x)*self.cell_size, self.cell_size, self.cell_size))
+                else:
+                    pygame.draw.rect(self.screen, pygame.Color('green'), ((y)*self.cell_size,(x)*self.cell_size, self.cell_size, self.cell_size))
+
+    def click(self, pos):
+        self.cells[pos[1]//self.cell_size][pos[0]//self.cell_size] = 1
+
+    def fill(self, randomize=False):
         if randomize:
-            cells = numpy.random.randint(2, size=(self.cell_width, self.cell_height))
+            cells = numpy.random.randint(2, size=(self.x, self.y))
         else:
-            cells = numpy.random.randint(1, size=(self.cell_width, self.cell_height))
+            cells = numpy.random.randint(1, size=(self.x, self.y))
         return cells   
 
     def tern(self, cells):
-        z=len(cells)
-        nextcells = numpy.random.randint(1, size=(z, z))
-        a = numpy.random.randint(1, size=(z+2, z+2))
-        b = numpy.random.randint(1, size=(z+2, z+2))
+        nextcells = numpy.random.randint(1, size=(self.x, self.y))
+        a = numpy.random.randint(1, size=(self.x+2, self.y+2))
+        b = numpy.random.randint(1, size=(self.x+2, self.y+2))
 
-        for x in range(0, z):
-            for y in range(0, z):
+        for x in range(0, self.x):
+            for y in range(0, self.y):
                 a[x+1,y+1] = cells[x,y]
 
-        for x in range(1, z+1):
-            for y in range(1, z+1):
+        for x in range(1, self.x+1):
+            for y in range(1, self.y+1):
                 c = 0
                 for i in range(x-1, x+2):
                     for j in range(y-1, y+2):
@@ -51,29 +61,18 @@ class GameOfLife:
                 else:
                     b[x, y] = a[x, y]
 
-        for x in range(0, z):
-            for y in range(0, z):
+        for x in range(0, self.x):
+            for y in range(0, self.y):
                 nextcells[x,y] = b[x+1,y+1]
 
         return(nextcells)
 
-    def draw_cell_list (self, cells):
-        for x in range (self.cell_width):
-            for y in range (self.cell_height):
-                if cells[x][y] == 0:
-                    pygame.draw.rect(self.screen, pygame.Color('white'), ((x)*self.cell_size,(y)*self.cell_size, self.cell_size, self.cell_size))
-                else:
-                    pygame.draw.rect(self.screen, pygame.Color('green'), ((x)*self.cell_size,(y)*self.cell_size, self.cell_size, self.cell_size))
-
-    def cell_click (self, pos):
-        self.cells[pos[0]//self.cell_size][pos[1]//self.cell_size] = 1
-  
     def run(self):
         pygame.init()
         clock = pygame.time.Clock()
-        pygame.display.set_caption('Game of Life')
+        pygame.display.set_caption('Life')
         self.screen.fill(pygame.Color('white'))
-        self.cells = self.cell_list(True)
+        self.cells = self.fill(True)
         running = True
         while running:
             self.cells = self.tern(self.cells)
@@ -82,13 +81,13 @@ class GameOfLife:
                     running = False
                 if event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
-                    self.cell_click(pos)
-            self.draw_cell_list(self.cells)
+                    self.click(pos)
+            self.draw(self.cells)
             self.draw_grid()
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
 
 if __name__ == '__main__':
-    game = GameOfLife()
+    game = Life()
     game.run()
